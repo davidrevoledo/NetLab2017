e9bccd5a327d10704a060d7c408267d49f5549d4
d-genix@outlook.com <d-genix@outlook.com>
2017-12-04 09:10:38 -0300

    HtmlHelpers

--- 30-11-Web/NorthWebApp/Domain/Territory.cs
+++ 30-11-Web/NorthWebApp/Domain/Territory.cs
@@ -1,28 +1,24 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Web.UI.WebControls;
 
 namespace NorthWebApp.Domain
 {
-    public partial class Territory
+    public class Territory
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Territory()
-        {
-            Employees = new HashSet<Employee>();
-        }
-
         [StringLength(20)]
         public string TerritoryID { get; set; }
 
         [Required]
-        [StringLength(50)]
+        //[StringLength(50)]
+        [MaxLength(50, ErrorMessage = "Flaco no pongas mas de 50 caracteres !")]
+        [Display(Name = "Descripcion")]
         public string TerritoryDescription { get; set; }
 
         public int RegionID { get; set; }
 
         public virtual Region Region { get; set; }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Employee> Employees { get; set; }
+        public virtual ICollection<Employee> Employees { get; set; } = new HashSet<Employee>();
     }
 }

4c82f6c8bc0f83aaaf70b462b662d2d007293240
d-genix@outlook.com <d-genix@outlook.com>
2017-12-04 10:52:09 -0300

    AspNet State

---
+++ 30-11-Web/MvcState/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcState.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //var example = Request.Cookies["example"].Value;
+            //var label = Session["session-test"].ToString();
+            //var cookie = (HttpCookie) Session["COOKIE"];
+
+            HttpContext.Application.Add("page-count", 1);
+
+            return View();
+        }
+
+        //[HttpGet]
+        //public ActionResult Logout()
+        //{
+        //    Session.Clear();
+
+        //    Session.remo
+        //    return View();
+        //}
+
+        [HttpGet]
+        public ActionResult RequestView()
+        {
+            var cookie = new HttpCookie("example", "ARG")
+            {
+                Expires = DateTime.Now.AddYears(1)
+            };
+
+
+            Response.Cookies.Add(cookie);
+
+            Session["COOKIE"] = cookie;
+            Session["session-test"] = "Net Lab";
+
+            return View(this.Request);
+        }
+    }
+}
--- 30-11-Web/NorthWebApp/HtmlHelpers/HtmlHelpersExtensions.cs
+++ 30-11-Web/NorthWebApp/HtmlHelpers/HtmlHelpersExtensions.cs
@@ -4,14 +4,14 @@ namespace NorthWebApp
 {
     public static class HtmlHelpersExtensions
     {
-        //public static MvcHtmlString Button(this HtmlHelper html, string label = "Create")
-        //{
-        //    var buttonHtml = $@"
-        //        <input type='submit' value='{label}' class='btn btn-default' />
-        //            ";
+        public static MvcHtmlString Button(this HtmlHelper html, string label = "Create")
+        {
+            var buttonHtml = $@"
+                <input type='submit' value='{label}' class='btn btn-default' />
+                    ";
 
-        //    return new MvcHtmlString(buttonHtml);
-        //}
+            return new MvcHtmlString(buttonHtml);
+        }
 
         public enum HtmlHTypes
         {

4da2c901b4ebb4d1bd10e53e83af4bfaff7e6342
d-genix@outlook.com <d-genix@outlook.com>
2017-12-06 10:59:55 -0300

    Check Session

--- Test/Test/Practica1/Controllers/HomeController.cs
+++ Test/Test/Practica1/Controllers/HomeController.cs
@@ -7,30 +7,62 @@ using Practica1.Filters;
 
 namespace Practica1.Controllers
 {
-    /*
+    /* 1)
      *  - Crear un sitio que cuente la cantidad de veces
      *    que se entro a la aplicacion en general.
           Mostrar en el footer
      */
+
+
+    //2 - Crear una pantalla donde se pida un input
+    //con el nombre de la persona y un boton y vaya a otra pantalla y aparezca el mensaje
+    //Bienvenido X(Nombre de la persona)
+
+    /* 3) - El nombre que se ingreso mostrarlo en el header a la derecha en todas las pantallas
+     *    - Si el usuario no ingreso el nombre, no permitirle el ingreso a ninguna otra pantalla
+     *      Si trata de ir a otra pantalla se redirigir a la pantalla de ingreso de nombre.
+     * 
+     */
+
+    /*
+     * 4) Mostrar un contador en el footer de cantidad de visitantes (diferenciados por session)
+     *    Agregar el menu logout para permitir volver a ingresar el nombre de usuario
+     *    El color del boton del login debe ser del color del menu
+     */
     [Contador]
     public class HomeController : Controller
     {
+        [Auth]
         public ActionResult Index()
         {
-
             return View();
         }
 
+        [Auth]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
             return View();
         }
 
+        [Auth]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginResult(string nombre)
+        {
+            HttpContext.Session["user"] = nombre;
+            ViewBag.nombre = nombre;
             return View();
         }
     }
---
+++ Test/Test/Practica1/Filters/AuthAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Practica1.Filters
+{
+    public class AuthAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            return httpContext.Session["user"] != null;
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary(new {controller = "Home", action = "Login"})
+            );
+        }
+    }
+}

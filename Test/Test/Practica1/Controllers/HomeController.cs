97c31632baae3f8137f6fa58b0000a7959a95968
d-genix@outlook.com <d-genix@outlook.com>
2017-12-06 09:15:00 -0300

    AspNet Ex 1

---
+++ Test/Test/Practica1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica1.Controllers
+{
+    /*
+     *  - Crear un sitio que cuente la cantidad de veces
+     *    que se entro a la aplicacion en general.
+          Mostrar en el footer
+     */
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var count = 1;
+
+            if (HttpContext.Application["count"] == null)
+            {
+                HttpContext.Application["count"] = count;
+            }
+            else
+            {
+                count = int.Parse(HttpContext.Application["count"].ToString()) + 1;
+                HttpContext.Application["count"] = count;
+            }
+
+            ViewBag.Count = count;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}

fdd76665656ec3cfa386b36ef93d38c0252c1fcc
d-genix@outlook.com <d-genix@outlook.com>
2017-12-07 09:02:14 -0300

    Ex 6

--- Test/Test/Practica1/Controllers/HomeController.cs
+++ Test/Test/Practica1/Controllers/HomeController.cs
@@ -38,6 +38,12 @@ namespace Practica1.Controllers
         (Puede ser en memoria)
         */
 
+
+    /*
+        6)
+        Ahora el sistema solo va a permitir el ingreso a la pantalla anterior si el usuario logeado es 'Admin'
+    */
+
     [Contador]
     public class HomeController : Controller
     {
